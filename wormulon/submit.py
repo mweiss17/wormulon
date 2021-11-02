99bf81ea0b91902e1898a6998011ca108d165bfc
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-11-02 13:54:00 -0400

    added kwargs

--- wormulon/submit.py
+++ wormulon/submit.py
@@ -6,21 +6,26 @@ import submitit
 from submitit.core.utils import CommandFunction
 
 @click.command(context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
+@click.argument('train_script')
 @click.argument('experiment_dir')
-def main(experiment_dir, **kwargs):
+@click.option('--mem_gb')
+@click.option('--cpus_per_task')
+@click.option('--slurm_gres')
+def main(train_script, experiment_dir, **kwargs):
+
     # create the submitit executor for creating and managing jobs
     ex = submitit.AutoExecutor(folder=os.path.join(experiment_dir, "Logs"))
 
     # setup the executor parameters based on the cluster location
     if ex.cluster == "slurm":
         ex.update_parameters(
-            mem_gb=16,
-            cpus_per_task=12,
-            timeout_min=1000,
+            mem_gb=kwargs.get("mem_gb", 8),
+            cpus_per_task=kwargs.get("cpus_per_task", 12),
+            timeout_min=kwargs.get("timeout_min", 12),
             tasks_per_node=1,
             nodes=1,
-            slurm_partition="long",
-            slurm_gres="gpu:rtx8000:1",
+            slurm_partition=kwargs.get("slurm_partition", "long"),
+            slurm_gres=kwargs.get("slurm_gres", None),
         )
     command = ["python3"] + sys.argv[1:]
     print(f"running: {command}")
