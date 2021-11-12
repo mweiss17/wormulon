2dbf65d8f742c69877e7f569b32b2bf87cfcb447
Martin Weiss <martin.clyde.weiss@gmail.com>
2021-11-12 12:21:23 -0500

    cleaning code

--- wormulon/tpu_submit.py
+++ wormulon/tpu_submit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import click
+import time
 import subprocess
 from typing import List
+from wormulon.utils import _upload_data_to_gcs
 
 
 def execute(command):
@@ -19,7 +20,15 @@ def get_node_ids(zone="us-central1-f") -> List[int]:
     return int_ids, error
 
 
-def create_tpu_node(node_id, network, subnetwork, network_range, tpu_type, preemptible):
+def create_tpu_node(
+    node_id,
+    network,
+    subnetwork,
+    network_range,
+    tpu_type,
+    preemptible=False,
+    asynch=False,
+):
 
     node_name = f"node-{node_id}"
     print(f"creating: {node_name}")
@@ -30,10 +39,12 @@ def create_tpu_node(node_id, network, subnetwork, network_range, tpu_type, preem
       --subnetwork {subnetwork} \
       --range {network_range} \
       --accelerator-type {tpu_type} \
-      --version v2-alpha \
-      --async"
+      --version v2-alpha"
+
     if preemptible:
         command += " --preemptible"
+    if asynch:
+        command += " --async"
 
     return execute(command.split())
 
@@ -78,7 +89,9 @@ def check_preempted(node_id):
     return output
 
 
-def tpu_submit_state(
+def tpu_submit(
+    bucket,
+    experiment_directory,
     trainer,
     training_state,
     network,
@@ -88,90 +101,102 @@ def tpu_submit_state(
     preemptible,
     **kwargs,
 ):
-    node_id = max(get_node_ids()) + 1
+    node_ids, error = get_node_ids()
+    node_id = max(node_ids) + 1
+    prefix = f"/{experiment_directory}/{str(time.time())}"
+    trainer_path = f"{prefix}/trainer.pt"
+    training_state_path = f"{prefix}/training_state.pt"
+    print(f"uploading training state and trainer to {prefix}.")
+    _upload_data_to_gcs(bucket, trainer_path, trainer.serialize())
+    _upload_data_to_gcs(bucket, training_state_path, training_state.serialize())
+
+    print(f"Currently, we have tpu nodes: {node_ids}, adding {node_id} now...")
 
     create_tpu_node(node_id, network, subnetwork, network_range, tpu_type, preemptible)
 
     cmd = (
         f"cd ~/; git clone https://github.com/mweiss17/polytax.git; "
-        f"pip install -e polytax; "
-        f"python3 polytax/src/polytax/train.py"
+        f"pip install -e polytax[xla]; "
     )
     output, error = run_ssh_cmd_on_tpu_node(node_id, cmd)
 
+    cmd = f"cd ~/polytax/src/polytax; python3 train.py {bucket} {trainer_path} {training_state_path} "
+    output, error = run_ssh_cmd_on_tpu_node(node_id, cmd)
 
-@click.command(
-    context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
-)
-@click.argument("root_id", default=1, type=int)
-@click.argument("num_nodes", default=1, type=int)
-@click.argument("exp_name", default="experiments/t5-xs-shakespeare", type=str)
-@click.argument("template_name", default="experiments/t5-xs-shakespeare", type=str)
-@click.argument("network", default="tpu-network", type=str)
-@click.argument("subnetwork", default="swarm-2", type=str)
-@click.argument("network_range", default="192.169.0.0/29", type=str)
-@click.argument("tpu_type", default="v2-8", type=str)
-@click.option("-p", "--preemptible", is_flag=True, default=False)
-def tpu_submit(
-    root_id,
-    num_nodes,
-    exp_name,
-    template_name,
-    network,
-    subnetwork,
-    network_range,
-    tpu_type,
-    preemptible,
-    **kwargs,
-):
-    command = f"gcloud alpha compute tpus tpu-vm create node-{root_id} \
-      --zone us-central1-f \
-      --network {network} \
-      --subnetwork {subnetwork} \
-      --range {network_range} \
-      --accelerator-type {tpu_type} \
-      --version v2-alpha"
-    if preemptible:
-        command += f" --preemptible"
 
-    execute(command.split())
-
-    # Get internal-ip for the control plane node
-    command = (
-        f"gcloud alpha compute tpus describe node-{root_id} --format=get(ipAddress)"
-    )
-    output, error = execute(command.split())
-
-    # Boot all the other nodes (if booting more than one
-    for i in range(1, num_nodes):
-        node_name = f"node-{i+root_id}"
-        print(f"creating: {node_name}")
-        # Spool up a TPU node
-        command = f"gcloud alpha compute tpus tpu-vm create {node_name} \
-          --zone us-central1-f \
-          --network {network} \
-          --subnetwork {subnetwork} \
-          --range {range} \
-          --accelerator-type {tpu_type} \
-          --version v2-alpha \
-          --async"
-        if preemptible:
-            command += " --preemptible"
-
-        execute(command.split())
-
-    # ssh into each node and setup polytax
-    for i in range(0, num_nodes):
-        node_name = f"node-{i+root_id}"
-        print(f"running start script on: {node_name}")
-        command = (
-            f"gcloud alpha compute tpus tpu-vm ssh "
-            f"{node_name} "
-            f"--zone us-central1-f "
-            f"--command "
-        )
-        command = command.split()
-        command.append(
-            f"cd ~/; git clone https://github.com/mweiss17/polytax.git; chmod 755 polytax/src/polytax/scripts/*.sh; ./polytax/src/polytax/scripts/launch_tpu.sh {i} localhost {exp_name} {template_name}"
-        )
-        output, error = execute(command)
+#
+# @click.command(
+#     context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
+# )
+# @click.argument("root_id", default=1, type=int)
+# @click.argument("num_nodes", default=1, type=int)
+# @click.argument("exp_name", default="experiments/t5-xs-shakespeare", type=str)
+# @click.argument("template_name", default="experiments/t5-xs-shakespeare", type=str)
+# @click.argument("network", default="tpu-network", type=str)
+# @click.argument("subnetwork", default="swarm-2", type=str)
+# @click.argument("network_range", default="192.169.0.0/29", type=str)
+# @click.argument("tpu_type", default="v2-8", type=str)
+# @click.option("-p", "--preemptible", is_flag=True, default=False)
+# def tpu_submit(
+#     root_id,
+#     num_nodes,
+#     exp_name,
+#     template_name,
+#     network,
+#     subnetwork,
+#     network_range,
+#     tpu_type,
+#     preemptible,
+#     **kwargs,
+# ):
+#     command = f"gcloud alpha compute tpus tpu-vm create node-{root_id} \
+#       --zone us-central1-f \
+#       --network {network} \
+#       --subnetwork {subnetwork} \
+#       --range {network_range} \
+#       --accelerator-type {tpu_type} \
+#       --version v2-alpha"
+#     if preemptible:
+#         command += f" --preemptible"
+#
+#     execute(command.split())
+#
+#     # Get internal-ip for the control plane node
+#     command = (
+#         f"gcloud alpha compute tpus describe node-{root_id} --format=get(ipAddress)"
+#     )
+#     output, error = execute(command.split())
+#
+#     # Boot all the other nodes (if booting more than one
+#     for i in range(1, num_nodes):
+#         node_name = f"node-{i+root_id}"
+#         print(f"creating: {node_name}")
+#         # Spool up a TPU node
+#         command = f"gcloud alpha compute tpus tpu-vm create {node_name} \
+#           --zone us-central1-f \
+#           --network {network} \
+#           --subnetwork {subnetwork} \
+#           --range {range} \
+#           --accelerator-type {tpu_type} \
+#           --version v2-alpha \
+#           --async"
+#         if preemptible:
+#             command += " --preemptible"
+#
+#         execute(command.split())
+#
+#     # ssh into each node and setup polytax
+#     for i in range(0, num_nodes):
+#         node_name = f"node-{i+root_id}"
+#         print(f"running start script on: {node_name}")
+#         command = (
+#             f"gcloud alpha compute tpus tpu-vm ssh "
+#             f"{node_name} "
+#             f"--zone us-central1-f "
+#             f"--command "
+#         )
+#         command = command.split()
+#         command.append(
+#             f"cd ~/; git clone https://github.com/mweiss17/polytax.git; chmod 755 polytax/src/polytax/scripts/*.sh; ./polytax/src/polytax/scripts/launch_tpu.sh {i} localhost {exp_name} {template_name}"
+#         )
+#         output, error = execute(command)
--- wormulon/utils.py
+++ wormulon/utils.py
@@ -1,3 +1,5 @@
+import io
+import torch
 from google.cloud import storage
 
 
@@ -21,26 +23,19 @@ class JobStatus:
             return "UNKNOWN"
 
 
-def _upload_blob_to_gcs_from_path(local_path, gcs_path):
-    """Uploads a file to GCS bucket"""
-    client = storage.Client()
-    blob = storage.blob.Blob.from_string(gcs_path)
-    blob.bucket._client = client
-    blob.upload_from_filename(local_path)
-
-
-def _upload_data_to_gcs(data, gcs_path):
+def _upload_data_to_gcs(bucket, remote_file_path, data):
     """Uploads a blob to GCS bucket"""
     client = storage.Client()
-    blob = storage.blob.Blob.from_string(gcs_path)
+    blob = storage.blob.Blob.from_string(bucket + "/" + remote_file_path)
     blob.bucket._client = client
     blob.upload_from_string(data)
 
 
-def _read_blob_gcs(bucket, remote_file_path, destination):
+def _read_blob_gcs(bucket, remote_file_path):
     """Downloads a file from GCS to local directory"""
     client = storage.Client()
     bucket = client.get_bucket(bucket)
     blob = bucket.get_blob(remote_file_path)
-    blob.download_to_filename(destination)
-    return os.read(destination)
+    bytes = blob.download_as_bytes()
+    buffer = io.BytesIO(bytes)
+    return buffer
